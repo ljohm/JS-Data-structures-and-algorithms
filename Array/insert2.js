7c8c177cef5018ac69943b2dc3d4c0803e0b130d
yoon1111116 <yunn1209@gmail.com>
2021-04-22 09:35:31 +0900

    array insert test

---
+++ Array/insert2.js
@@ -0,0 +1,12 @@
+// 88. Merge Sorted Array
+
+let merge = function (nums1, m, nums2, n) {
+  for (let i = 0; i < n; i++) {
+    nums1[m + i] = nums2[i];
+  }
+  return nums1.sort((a, b) => {
+    return a - b;
+  });
+};
+
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
