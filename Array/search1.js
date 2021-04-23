a31f0c92c550530e497b224e1ebe52e9e0c664e6
yoon1111116 <yunn1209@gmail.com>
2021-04-23 22:16:10 +0900

    array search test

---
+++ Array/search1.js
@@ -0,0 +1,20 @@
+// 1346. Check If N and Its Double Exist
+
+let checkIfExist = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.indexOf(arr[i] * 2) !== -1 && arr[i] !== 0) {
+      return true;
+    } else if (arr[i] === 0 && arr.indexOf(0) !== i) {
+      return true; // 값이 0인 원소가 두개일 경우 ex) [0, 0]
+    }
+  }
+  return false;
+};
+
+// indexOf()도 있고 includes()도 있음
+// includes()는 boolean으로 리턴.
+
+let arr = [-2, 0, 10, -19, 4, 6, -8];
+// console.log(checkIfExist(arr));
+let arr2 = [0, 0];
+console.log(arr2.indexOf(0));
---
+++ Array/search2.js
@@ -0,0 +1,36 @@
+// 941. Vaiid Mountain Array
+//
+
+let validMountainArray = function (arr) {
+  if (arr.length < 3) {
+    return false;
+  }
+
+  let prev = arr[0];
+  let index = 1;
+  let increase = 0;
+  let decrease = 0;
+
+  while (prev < arr[index]) {
+    prev = arr[index];
+    increase++;
+    index++;
+    // 중요!! prev가 원소값보다 작거나 커야만
+    // index 증가
+  }
+
+  while (prev > arr[index]) {
+    prev = arr[index];
+    decrease++;
+    index++;
+  }
+
+  return index === arr.length && increase > 0 && decrease > 0;
+};
+
+let arr = [2, 0, 2];
+console.log(validMountainArray(arr));
+
+// Math.max() 메소드 사용법
+// let max = Math.max(...arr); => arr의 max값
+// let maxIndex = arr.indexOf(max); => max값의 위치
